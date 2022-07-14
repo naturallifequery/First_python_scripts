6dd10087a2e19dbce1bcaf90bf8e56957a56fd3a
naturallifequery <109215989+naturallifequery@users.noreply.github.com>
2022-07-14 08:56:56 +0100

    Add files via upload

---
+++ for_range_example.py
@@ -0,0 +1,3 @@
+print ('My name is')
+for i in range(10,20, +2):  #for i variable in range 10-20 (in steps of 2) 
+    print('Jimmy Five Times ' + str(i))
---
+++ forloop_rewritten_as_while.py
@@ -0,0 +1,4 @@
+myname = 0
+while myname < 5 :
+    print('My name is Jimmy Five Times ' + str(myname))
+    myname = myname + 1
